a9bbd7879d16ab58132f2fdc96bdefd0c48bd3bc
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-06 15:50:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "public_ip" {
   value       = "${aws_instance.tomcat.*.public_ip}"
   description = "PrivateIP address details"
 }  
+
+output "ami_id" {
+  value       = "${data.aws_ami.ubuntu.id}"
+  description = "AMI ID depends on the instance type and region in which you're launching your stack. And IDs can change regularly, such as when an AMI is updated with software updates."
+}
