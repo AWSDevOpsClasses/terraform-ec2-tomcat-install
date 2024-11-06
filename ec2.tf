1b3eb3982bc559a0e885250e64ae5106d18fa67a
srinivasateja <srinivas.21may@gmail.com>
2024-11-06 13:54:00 +0530

    terraform ec2 userdata tomcat installation

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "tomcat" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t3.medium"
+  key_name      = "nvirginia-devops"
+  count = 1
+  vpc_security_group_ids = [
+    "sg-0a4aad1f3fd186745"
+  ]
+  subnet_id = "subnet-0f7f1a84a20b5d114"
+  tags = {
+    Name = "${var.prefix}${count.index}"
+  }
+ }
+
+}
