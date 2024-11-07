92f4f2b04e8b4147f20bfa5b35e571756633d7f6
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-07 08:42:44 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,6 +9,11 @@ output "public_ip" {
 }  
 
 output "ami_id" {
-  value       = "${data.aws_ami.ubuntu.id}"
+  value       = "${data.aws_ami.ubuntu.*.id}"
   description = "AMI ID depends on the instance type and region in which you're launching your stack. And IDs can change regularly, such as when an AMI is updated with software updates."
+}
+
+output "sg_id" {
+  value       = "${aws_security_group.tomcatsg.*.id}"
+  description = "SecurityGroup ID"
 }
